f756b13e07729ff75dc6c9bf4d09fe3b03879423
yongss <bbl737898@gmail.com>
2025-09-10 16:18:53 +0900

    feat: 테스트 코드 데이터 롤백 @Transactional 작성
    - @Transactional 작성

--- itemservice-db/src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
+++ itemservice-db/src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
@@ -11,26 +11,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Transactional
 @SpringBootTest
 class ItemRepositoryTest {
 
     @Autowired
     ItemRepository itemRepository;
 
-    @Autowired
-    PlatformTransactionManager transactionManager;
-    TransactionStatus status;
-
-    @BeforeEach
-    void beforeEach() {
-        status = transactionManager.getTransaction(new DefaultTransactionDefinition());
-    }
+//    @Autowired
+//    PlatformTransactionManager transactionManager;
+//    TransactionStatus status;
+//
+//    @BeforeEach
+//    void beforeEach() {
+//        status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+//    }
 
     @AfterEach
     void afterEach() {
@@ -38,7 +40,7 @@ class ItemRepositoryTest {
         if (itemRepository instanceof MemoryItemRepository) {
             ((MemoryItemRepository) itemRepository).clearStore();
         }
-        transactionManager.rollback(status);
+//        transactionManager.rollback(status);
     }
 
     @Test
