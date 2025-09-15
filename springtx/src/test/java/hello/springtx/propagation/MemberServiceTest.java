4d6f944b2336ea5b93dcd13743a2f88bea7ad711
yongss <bbl737898@gmail.com>
2025-09-15 10:58:57 +0900

    feat: Spring transaction 전파
    - 트랜잭션 단일 트랜잭션 테스트 코드 작성

--- springtx/src/test/java/hello/springtx/propagation/MemberServiceTest.java
+++ springtx/src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -56,4 +56,22 @@ class MemberServiceTest {
         assertTrue(memberRepository.find(username).isPresent());
         assertTrue(logRepository.find(username).isEmpty());
     }
+
+    /**
+     * memberService    @Transactional: ON
+     * memberRepository @Transactional: OFF
+     * logRepository    @Transactional: OFF
+     */
+    @Test
+    void singleTx() {
+
+        //given
+        String username = "outerTxOff_success";
+
+        //when
+        memberService.joinV1(username);
+
+        assertTrue(memberRepository.find(username).isPresent());
+        assertTrue(logRepository.find(username).isPresent());
+    }
 }
